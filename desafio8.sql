83a49704c3decb42641cb2d735b2081e4c043802
igordmf <igordanielmf@gmail.com>
2021-09-17 00:43:07 -0300

    Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+  BEFORE DELETE ON SpotifyClone.usuarios
+  FOR EACH ROW
+  BEGIN
+    DELETE * FROM SpotifyClone.usuario_artista AS u_a
+    WHERE u_a.usuario_id = OLD.usuario_id;
+
+    DELETE * FROM SpotifyClone.usuario_cancao AS u_c
+    WHERE u_c.usuario_id = OLD.usuario_id;
+  END $$
+
+  DELIMITER ;
