48c01b630ce52f4e2f04a01f47d4cd33d5f87cb4
igordmf <igordanielmf@gmail.com>
2021-09-17 00:50:58 -0300

    Requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,10 +3,10 @@ CREATE TRIGGER trigger_usuario_delete
   BEFORE DELETE ON SpotifyClone.usuarios
   FOR EACH ROW
   BEGIN
-    DELETE * FROM SpotifyClone.usuario_artista AS u_a
+    DELETE FROM SpotifyClone.usuario_artista AS u_a
     WHERE u_a.usuario_id = OLD.usuario_id;
 
-    DELETE * FROM SpotifyClone.usuario_cancao AS u_c
+    DELETE FROM SpotifyClone.usuario_cancao AS u_c
     WHERE u_c.usuario_id = OLD.usuario_id;
   END $$
 
