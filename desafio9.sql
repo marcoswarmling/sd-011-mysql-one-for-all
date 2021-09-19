5f2eafc817d1c7bd31a92f9a25836721ec51098e
samuelvanguarda <samuelvanguarda@gmail.com>
2021-09-19 18:31:23 -0300

    Requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,12 @@
+DELIMITER $$ 
+CREATE PROCEDURE albuns_do_artista (IN artista varchar(35))
+BEGIN
+SELECT a.artista AS 'artista',
+  al.album AS 'album'
+FROM SpotifyClone.artistas AS a
+  INNER JOIN SpotifyClone.albuns AS al
+  ON a.artista_id = al.artista_id
+WHERE a.artista = artista
+ORDER BY `album`;
+END $$
+DELIMITER ;
