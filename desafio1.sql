e423f017ee82578afbe4ae0c45f03f94292fb95a
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-09-19 08:02:47 -0300

    Fixing desafio1

--- desafio1.sql
+++ desafio1.sql
@@ -5,54 +5,55 @@ CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
 
+
 CREATE TABLE planos (
-plano_id INT UNSIGNED	primary key		 AUTO_INCREMENT,
-valor DECIMAL(3,2) UNSIGNED NOT NULL,
+plano_id INT 	primary key		 AUTO_INCREMENT,
+valor DECIMAL(3,2)  NOT NULL,
 plano varchar(70) NOT NULL
 )
 ENGINE = InnoDB;
 
 
 CREATE TABLE usuarios (
-usuario_id INTEGER UNSIGNED	primary key	 NOT NULL AUTO_INCREMENT,
-idade TINYINT UNSIGNED NOT NULL,
+usuario_id INT	primary key	 AUTO_INCREMENT,
+idade TINYINT NOT NULL,
 nomeUsuario varchar(70) NOT NULL,
-plano_id INT UNSIGNED NOT NULL,
+plano_id INT NOT NULL,
 FOREIGN KEY (plano_id) REFERENCES planos(plano_id)
 )
 ENGINE = InnoDB;
 
 CREATE TABLE artistas (
-artista_id INT UNSIGNED primary key				AUTO_INCREMENT,
+artista_id INT  primary key				AUTO_INCREMENT,
 nomeArtista varchar(70) NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE albuns (
-album_id INT UNSIGNED	primary key		 AUTO_INCREMENT,
+album_id INT 	primary key		 AUTO_INCREMENT,
 nomeAlbum varchar(70) NOT NULL,
-artista_id INT UNSIGNED NOT NULL,
+artista_id INT  NOT NULL,
 FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 )
 ENGINE = InnoDB;
 
 CREATE TABLE cancoes (
-cancao_id INT UNSIGNED	primary key		 AUTO_INCREMENT,	
-album_id INT UNSIGNED NOT NULL,
+cancao_id INT 	primary key		 AUTO_INCREMENT,	
+album_id INT  NOT NULL,
 nomeCancao varchar(70) NOT NULL,
 FOREIGN KEY (album_id) REFERENCES albuns(album_id)
 ) ENGINE = InnoDB;
 
 CREATE TABLE historico_de_reproducoes (
-usuario_id INT UNSIGNED NOT NULL,
-cancao_id INT UNSIGNED NOT NULL,
+usuario_id INT NOT NULL,
+cancao_id INT NOT NULL,
 CONSTRAINT PRIMARY KEY(usuario_id, cancao_id),
 FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
 FOREIGN KEY (cancao_id) REFERENCES cancoes(cancao_id)
 ) ENGINE = InnoDB;
 
 CREATE TABLE seguindo_artistas (
-usuario_id INT UNSIGNED NOT NULL,
-artista_id INT UNSIGNED NOT NULL,
+usuario_id INT NOT NULL,
+artista_id INT NOT NULL,
 CONSTRAINT PRIMARY KEY(usuario_id, artista_id),
 FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
 FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
