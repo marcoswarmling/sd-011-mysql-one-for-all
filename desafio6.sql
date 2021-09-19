427b018050abdd19ae89c08009d5378f40dc527d
Rafael Schmitz <schimitaum@gmail.com>
2021-09-19 01:52:53 -0300

    sim

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 CREATE VIEW faturamento_atual AS
-SELECT FORMAT(MIN(valor), 2) AS faturamento_minimo,
+SELECT FORMAT(MIN(price), 2) AS faturamento_minimo,
 FORMAT(MAX(price), 2) AS faturamento_maximo,
 FORMAT(AVG(CEIL (price)), 2) AS faturamento_medio,
 FORMAT(SUM(price), 2) AS faturamento_total
