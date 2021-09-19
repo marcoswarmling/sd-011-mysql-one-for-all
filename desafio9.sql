d78cb99146a6dc8d049cc0d3592ddf090b827ca0
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-09-19 10:57:21 -0300

    Corrigindo cc desafio9

--- desafio9.sql
+++ desafio9.sql
@@ -4,12 +4,12 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista (in nomeArtista varchar(70))
 BEGIN
-	SELECT artistas.nomeArtista as artista, albuns.nomeAlbum as album
-	from SpotifyClone.artistas as artistas
-	INNER JOIN SpotifyClone.albuns as albuns
-	on artistas.artista_id = albuns.artista_id
-    where artistas.nomeArtista = nomeArtista
-	ORDER BY `album` ASC;
+SELECT artistas.nomeArtista as artista, albuns.nomeAlbum as album
+from SpotifyClone.artistas as artistas
+INNER JOIN SpotifyClone.albuns as albuns
+on artistas.artista_id = albuns.artista_id
+where artistas.nomeArtista = nomeArtista
+ORDER BY `album` ASC;
 END $$
 
 DELIMITER ;
