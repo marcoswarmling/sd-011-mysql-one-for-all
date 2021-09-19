9598bb52e8394cfb799c700fa74141587807a19d
Arlesson Moura <arlessonmss@gmail.com>
2021-09-18 21:30:03 -0300

    Quem sabe qual é o erro?

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 USE SpotifyClone;
 CREATE VIEW top_3_artistas AS
-SELECT A.artista AS artista,
-COUNT(ARS.usuario) AS seguidores
-FROM artistas_seguindo AS ARS
-INNER JOIN artistas AS A
-ON  A.artista_id = ARS.artista 
-GROUP BY ARS.artista
-ORDER BY COUNT(ARS.usuario) DESC, artista
-LIMIT 3;
+	SELECT A.artista AS artista,
+	COUNT(ARS.usuario) AS seguidores
+	FROM artistas_seguindo AS ARS
+	INNER JOIN artistas AS A
+	ON  A.artista_id = ARS.artista 
+	GROUP BY ARS.artista
+	ORDER BY COUNT(ARS.usuario) DESC, artista
+	LIMIT 3;
