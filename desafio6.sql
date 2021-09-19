5a5034ba488770d8d53e55c1baaca80a7e05b905
Rafael Schmitz <schimitaum@gmail.com>
2021-09-19 01:48:10 -0300

    agora sim

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS
 SELECT FORMAT(MIN(valor), 2) AS faturamento_minimo,
-FORMAT(MAX(valor), 2) AS faturamento_maximo,
-FORMAT(AVG(CEIL (valor)), 2) AS faturamento_medio,
-FORMAT(SUM(valor), 2) AS faturamento_total
+FORMAT(MAX(price), 2) AS faturamento_maximo,
+FORMAT(AVG(CEIL (price)), 2) AS faturamento_medio,
+FORMAT(SUM(price), 2) AS faturamento_total
 FROM plans AS p
 INNER JOIN users AS u 
 ON p.plans_id = u.plans_id;
