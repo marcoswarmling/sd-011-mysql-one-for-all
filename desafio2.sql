8c5793a0fcce8b6a9cb6c23523b620c73b8c043a
samuelvanguarda <samuelvanguarda@gmail.com>
2021-09-19 17:32:25 -0300

    Requisitos 1,2,3,4 e 5 corrigidos

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,5 @@
-CREATE VIEW estatisticas_musicais AS
-SELECT COUNT(so.song_id) AS 'cancoes',
-  (
-    SELECT COUNT(ar.artist_id)
-    FROM SpotifyClone.artists AS ar
-  ) AS 'artistas',
-  (
-    SELECT COUNT(ab.album_id)
-    FROM SpotifyClone.albums AS ab
-  ) AS 'albuns'
-FROM SpotifyClone.songs AS so;
+CREATE VIEW estatisticas_musicais AS SELECT
+(SELECT COUNT(cancao) FROM SpotifyClone.cancoes) AS "cancoes",
+(SELECT COUNT(artista) FROM SpotifyClone.artistas) AS "artistas",
+(SELECT COUNT(album) FROM SpotifyClone.albuns) AS "albuns";
+
