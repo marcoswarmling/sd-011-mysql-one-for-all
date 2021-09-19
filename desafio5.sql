0e3d8480bac341101075520cb06b933f38b7fdbe
Rafael Schmitz <schimitaum@gmail.com>
2021-09-19 01:27:59 -0300

    5 agora foi

--- desafio5.sql
+++ desafio5.sql
@@ -2,9 +2,9 @@ CREATE VIEW top_2_hits_do_momento AS
 SELECT
 (SELECT m.music 
 FROM  musics AS m
-WHERE f.music_id = m.music_id) AS cancao,
+WHERE pm.music_id = m.music_id) AS cancao,
 COUNT(user_id) AS reproducoes
-FROM followers AS f
-GROUP BY f.music_id
+FROM played_musics AS pm
+GROUP BY pm.music_id
 ORDER BY reproducoes DESC, cancao
 LIMIT 2;
