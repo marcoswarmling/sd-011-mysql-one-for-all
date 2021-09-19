b8048da40117d0bfc7c681d2b4505219f0e521cd
Thiago Gomes <thiagog.macedom@gmail.com>
2021-09-19 19:45:04 -0300

    requisito 2 refatorando

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT
 (SELECT COUNT(*) FROM Cancoes) as cancoes,
-(SELECT COUNT(*) FROM Artistas) as Artistas,
+(SELECT COUNT(*) FROM Artistas) as artistas,
 (SELECT COUNT(*) FROM Albuns) as albuns;
