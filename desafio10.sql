fa2e99c2d3ae36ea8b0dd2e32d40ff9f6cdf64f4
FredericoCanoeira <fcanoeira@gmail.com>
2021-09-20 17:30:46 +0100

    fixing error codeclimate

--- desafio10.sql
+++ desafio10.sql
@@ -5,10 +5,10 @@ CREATE FUNCTION SpotifyClone.quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 
 BEGIN
-    DECLARE quantidadeTotal INT;
-	SELECT COUNT(*) FROM SpotifyClone.histories 
-    WHERE user_id = id INTO quantidadeTotal;
-	RETURN quantidadeTotal;
+DECLARE quantidadeTotal INT;
+SELECT COUNT(*) FROM SpotifyClone.histories 
+WHERE user_id = id INTO quantidadeTotal;
+RETURN quantidadeTotal;
 
 END $$
 
