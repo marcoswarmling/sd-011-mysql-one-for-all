e7ba7f7039b56485e47a0e15c26967c5177ded54
Thiago Gomes <thiagog.macedom@gmail.com>
2021-09-20 18:23:50 -0300

    requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN ARTISTA VARCHAR(20))
+BEGIN
+SELECT ar.nome AS 'artista',
+al.nome AS 'album'
+FROM SpotifyClone.Artistas ar
+INNER JOIN SpotifyClone.Albuns al
+ON ar.artista_id = al.artista
+WHERE ar.nome = ARTISTA;
+END $$
+DELIMITER ;
