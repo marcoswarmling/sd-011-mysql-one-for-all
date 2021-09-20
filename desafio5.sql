3ddfe341089b00bbca6fd75075c5fa683abcbfd2
Thiago Gomes <thiagog.macedom@gmail.com>
2021-09-19 22:05:19 -0300

    requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT c.nome AS cancao,
+COUNT(*) AS reproducoes
+FROM SpotifyClone.Historico_reproducao AS h
+INNER JOIN SpotifyClone.Cancoes as c ON c.cancao_id = h.cancao_id
+GROUP BY h.cancao_id
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
