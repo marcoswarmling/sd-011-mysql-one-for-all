40d9075a630981c3013be39cc2369e2899045879
FredericoCanoeira <fcanoeira@gmail.com>
2021-09-20 17:24:21 +0100

    implementing desafio11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+CREATE VIEW cancoes_premium AS SELECT s.song AS 'nome',
+COUNT(*) AS 'reproducoes' FROM SpotifyClone.songs AS s
+INNER JOIN SpotifyClone.histories AS h
+ON s.song_id = h.song_id
+INNER JOIN SpotifyClone.users AS u
+ON h.user_id = u.user_id
+INNER JOIN SpotifyClone.subscriptions AS sb
+ON u.subscription_id = sb.subscription_id
+WHERE subscription IN ('Familiar', 'Universitário')
+GROUP BY nome
+ORDER BY nome;
