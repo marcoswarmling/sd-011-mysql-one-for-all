475cfeab1b78b2e9be15956ad583867fc3feadbb
FredericoCanoeira <fcanoeira@gmail.com>
2021-09-20 16:19:42 +0100

    implementing desafio5

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT s.song AS 'cancao',
+COUNT(h.song_id) AS 'reproducoes'
+FROM SpotifyClone.songs AS s
+INNER JOIN SpotifyClone.histories AS h
+ON s.song_id = h.song_id
+GROUP BY `cancao`
+ORDER BY `reproducoes` DESC, `cancao`
+LIMIT 2;
