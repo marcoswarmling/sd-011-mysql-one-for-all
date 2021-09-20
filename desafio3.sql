4c09d9c122a54f9eecc91c78f440136bc8166509
FredericoCanoeira <fcanoeira@gmail.com>
2021-09-20 16:09:35 +0100

    implementing desafio3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT u.`user` AS 'usuario', s.song AS 'nome' 
+FROM SpotifyClone.users AS u
+INNER JOIN SpotifyClone.histories AS h
+ON u.user_id = h.user_id
+INNER JOIN SpotifyClone.songs AS s
+ON h.song_id = s.song_id 
+ORDER BY `usuario`, `nome`;
