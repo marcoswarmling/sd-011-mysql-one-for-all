bd232a9d65ac5d1b6db2feeefedb606f80fba1c1
FredericoCanoeira <fcanoeira@gmail.com>
2021-09-20 18:19:48 +0100

    fixing error evaluetor

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,10 @@
-CREATE VIEW cancoes_premium AS SELECT s.song AS 'nome',
-COUNT(*) AS 'reproducoes' FROM SpotifyClone.songs AS s
+CREATE VIEW cancoes_premium AS 
+SELECT s.song AS 'nome',
+COUNT(h.song_id) AS 'reproducoes' FROM SpotifyClone.songs AS s
 INNER JOIN SpotifyClone.histories AS h
 ON s.song_id = h.song_id
 INNER JOIN SpotifyClone.users AS u
 ON h.user_id = u.user_id
-INNER JOIN SpotifyClone.subscriptions AS sb
-ON u.subscription_id = sb.subscription_id
-WHERE subscription IN ('Familiar', 'Universitário')
+AND u.user_id IN (2, 3)
 GROUP BY nome
 ORDER BY nome;
--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,7 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT u.`user` AS 'usuario', s.song AS 'nome' 
 FROM SpotifyClone.histories AS h
+INNER JOIN SpotifyClone.songs AS s ON h.song_id = s.song_id
 INNER JOIN SpotifyClone.users AS u
 ON u.user_id = h.user_id
-INNER JOIN SpotifyClone.songs AS s
-ON h.song_id = s.song_id 
 ORDER BY usuario, nome;
