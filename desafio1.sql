9933a32ddec7e8c8c1f4d1a4478bf637d84bf2c8
FredericoCanoeira <fcanoeira@gmail.com>
2021-09-20 02:52:45 +0100

    fixing code climate error desafio1

--- desafio1.sql
+++ desafio1.sql
@@ -41,10 +41,10 @@ CREATE TABLE artists(
 
 INSERT INTO artists (artist_id, artist)
 VALUES
-   (1, 'Walter Phoenix'),
-   (2, 'Peter Strong'),
-   (3, 'Lance Day'),
-   (4, 'Freedie Shannon');
+    (1, 'Walter Phoenix'),
+    (2, 'Peter Strong'),
+    (3, 'Lance Day'),
+    (4, 'Freedie Shannon');
 
 CREATE TABLE albums(
     album_id INT NOT NULL AUTO_INCREMENT,
@@ -74,24 +74,24 @@ VALUES
 
 INSERT INTO songs (song_id, song, album_id, artist_id)
 VALUES
-	(1, 'Soul For Us', 1, 1),
-	(2,	'Reflections Of Magic', 1, 1),
-	(3,	'Dance With Her Own', 1, 1),
-	(4,	'Troubles Of My Inner Fire', 2,	1),
-	(5, 'Time Fireworks', 2, 1),
-	(6,	'Magic Circus',	3, 2),
-	(7,	'Honey, So Do I', 3, 2),
-	(8,	'Sweetie, Let’s Go Wild',	3, 2),
-	(9,	'She Knows', 3,	2),
-	(10, 'Fantasy For Me', 4, 3),
-	(11, 'Celebration Of More', 4, 3),
-	(12, 'Rock His Everything', 4, 3),
-	(13, 'Home Forever', 4,	3),
-	(14, 'Diamond Power', 4,	3),
-	(15, 'Honey, Let’s Be Silly', 4, 3),
-	(16, 'Thang Of Thunder',	5, 4),
-	(17, 'Words Of Her Life', 5,	4),
-	(18, 'Without My Streets', 5, 4);
+    (1, 'Soul For Us', 1, 1),
+    (2,	'Reflections Of Magic', 1, 1),
+    (3,	'Dance With Her Own', 1, 1),
+    (4,	'Troubles Of My Inner Fire', 2,	1),
+    (5, 'Time Fireworks', 2, 1),
+    (6,	'Magic Circus',	3, 2),
+    (7,	'Honey, So Do I', 3, 2),
+    (8,	'Sweetie, Let’s Go Wild',	3, 2),
+    (9,	'She Knows', 3,	2),
+    (10, 'Fantasy For Me', 4, 3),
+    (11, 'Celebration Of More', 4, 3),
+    (12, 'Rock His Everything', 4, 3),
+    (13, 'Home Forever', 4,	3),
+    (14, 'Diamond Power', 4,	3),
+    (15, 'Honey, Let’s Be Silly', 4, 3),
+    (16, 'Thang Of Thunder',	5, 4),
+    (17, 'Words Of Her Life', 5,	4),
+    (18, 'Without My Streets', 5, 4);
   
   CREATE TABLE `following`(
     user_id INT NOT NULL,
@@ -134,4 +134,3 @@ VALUES
   (4, 3),
   (4, 18),
   (4, 11);
-  
