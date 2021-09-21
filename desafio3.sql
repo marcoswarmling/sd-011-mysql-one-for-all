00b53a405b2906e1fde8d6d7dcdb55c1f22bd5ce
Diego Figueirêdo <8kball@gmail.com>
2021-09-20 22:32:47 -0300

    Typo

--- desafio3.sql
+++ desafio3.sql
@@ -5,5 +5,5 @@ FROM SpotifyClone.user AS u
 INNER JOIN SpotifyClone.rep_history AS h
 ON u.user_id = h.user_id
 INNER JOIN SpotifyClone.song AS s
-ON h.song_id = s.song_ig
+ON h.song_id = s.song_id
 ORDER BY `usuario`, `nome`;
