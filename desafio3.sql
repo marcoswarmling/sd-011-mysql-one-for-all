1eba2a21ccca934b3e8e6cf6efa05d83d0af81a6
Diego Figueirêdo <8kball@gmail.com>
2021-09-20 22:31:33 -0300

    Desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT u.name AS `usuario`,
+s.title AS `nome`
+FROM SpotifyClone.user AS u
+INNER JOIN SpotifyClone.rep_history AS h
+ON u.user_id = h.user_id
+INNER JOIN SpotifyClone.song AS s
+ON h.song_id = s.song_ig
+ORDER BY `usuario`, `nome`;
