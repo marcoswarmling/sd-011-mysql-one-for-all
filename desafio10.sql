5ba2105ad6615f78743020a895466a4bf2fb5433
Diego Figueirêdo <8kball@gmail.com>
2021-09-20 23:29:13 -0300

    Desafio10 Fix

--- desafio10.sql
+++ desafio10.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-DECLARE history_total;
+DECLARE history_total INT;
 SELECT COUNT(*) FROM SpotifyClone.rep_history
 WHERE user_id = id INTO history_total;
 RETURN history_total;
